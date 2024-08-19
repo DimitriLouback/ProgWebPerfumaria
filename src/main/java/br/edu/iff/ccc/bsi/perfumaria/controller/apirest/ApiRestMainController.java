432f0c00744d0c5d867198c0ac5d80e77e09ad3d
Dimitri <dimitrilouback91@gmail.com>
2024-08-19 12:24:37 -0300

    Estrutura do projeto

---
+++ src/main/java/br/edu/iff/ccc/bsi/perfumaria/controller/apirest/ApiRestMainController.java
@@ -0,0 +1,4 @@
+package br.edu.iff.ccc.bsi.perfumaria.controller.apirest;
+
+public class ApiRestMainController {
+}
---
+++ src/test/java/br/edu/iff/ccc/bsi/perfumaria/PerfumariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.bsi.perfumaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
