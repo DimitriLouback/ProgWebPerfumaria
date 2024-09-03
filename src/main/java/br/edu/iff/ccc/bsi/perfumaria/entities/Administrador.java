210ae67531f6718e54032f12e25e0247061e5a4f
Dimitri <dimitrilouback91@gmail.com>
2024-09-02 22:35:33 -0300

    criação da herança e relacionamento básico

--- src/main/java/br/edu/iff/ccc/bsi/perfumaria/entities/Administrador.java
+++ src/main/java/br/edu/iff/ccc/bsi/perfumaria/entities/Administrador.java
@@ -1,65 +1,24 @@
 package br.edu.iff.ccc.bsi.perfumaria.entities;
-
 import jakarta.persistence.*;
 
-import java.io.Serializable;
 
 @Entity
-public class Administrador implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    public Administrador(long id) {
-        this.id= id;
-
-    }
+public class Administrador extends Usuario {
 
     public Administrador() {
 
     }
 
-    @Column(name = "nome")
-    private String nome;
+    @Column(name = "senha_acesso")
+    private String senhaAcesso;
 
-    @Column(unique= true, name = "CPF")
-    private String CPF;
 
-    @Column(name = "celular")
-    private String celular;
-
-    public long getId() {
-        return id;
+    public String getSenhaAcesso() {
+        return senhaAcesso;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCPF() {
-        return CPF;
-    }
-
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
+    public void setSenhaAcesso(String senhaAcesso) {
+        this.senhaAcesso = senhaAcesso;
     }
 
 }
--- src/main/java/br/edu/iff/ccc/bsi/perfumaria/entities/Usuario.java
+++ src/main/java/br/edu/iff/ccc/bsi/perfumaria/entities/Usuario.java
@@ -6,28 +6,38 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "tb_usuarios")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
-private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-public Usuario(long id) {
+    public Usuario(long id) {
         this.id= id;
 
     }
 
-public Usuario() {
+    public Usuario() {
 
-}
+    }
 
-@Column(name = "username")
-private String username;
+    @Column(name = "nome")
+    private String username;
 
-@Column(unique= true, name = "email")
-private String email;
+    @Column(name = "senha")
+    private String senha;
+
+    @Column(unique= true, name = "email")
+    private String email;
+
+    @Column(name = "CPF")
+    private String CPF;
+
+    @Column(name = "celular")
+    private String celular;
 
     public long getId() {
         return id;
@@ -53,4 +63,27 @@ private String email;
         this.email = email;
     }
 
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
 }
